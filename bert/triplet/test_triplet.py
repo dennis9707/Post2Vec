aa8463e50afd06d9e2452a38b11e22b51fba6cc8
dennis9707 <he0707@hotmail.com>
2021-12-06 21:11:05 +0800

    feat() save a csv when testing

--- bert/triplet/test_triplet.py
+++ bert/triplet/test_triplet.py
@@ -13,7 +13,8 @@ from transformers import BertConfig
 from util.util import get_files_paths_from_directory
 from model.model import TBertTLarge
 from util.data_util import get_tag_encoder, get_fixed_tag_encoder, load_data_to_dataset, get_dataloader
-from util.eval_util import evaluate_batch
+from torch.utils.data import DataLoader
+import numpy as np
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
@@ -21,15 +22,85 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+import csv
+
+to_csv = []
+def evaluate_ori(pred, label, topk,mlb=None):
+    """
+    dimension of pred and label should be equal.
+    :param pred: a list of prediction
+    :param label: a list of true label
+    :param topk:
+    :return: a dictionary: {'precision': pre_k, 'recall': rec_k, 'f1': f1_k}
+    """
+    top_idx_list = sorted(range(len(pred)), key=lambda i: pred[i])[-topk:]
+    top_idx_one_hot = [0] * len(pred)
+    for idx in top_idx_list:
+        top_idx_one_hot[idx] = 1
+    num_of_true_in_topk = len([idx for idx in top_idx_list if label[idx] == 1])
+    # precision@k = #true label in topk / k
+    pre_k = num_of_true_in_topk / float(topk)
+    # recall@k = #true label in topk / #true label
+    num_of_true_in_all = sum(label)
+    if num_of_true_in_all == 0:
+        return -1,-1,-1
+    if num_of_true_in_all > topk:
+        rec_k = num_of_true_in_topk / float(topk)
+    else:
+        if num_of_true_in_all == 0:
+            return -1,-1,-1
+        rec_k = num_of_true_in_topk / float(num_of_true_in_all)
+    # f1@k = 2 * precision@k * recall@k / (precision@k + recall@k)
+    if pre_k == 0 and rec_k == 0:
+        f1_k = 0.0
+    else:
+        f1_k = 2 * pre_k * rec_k / (pre_k + rec_k)
+    # return {'precision': pre_k, 'recall': rec_k, 'f1': f1_k}
+    new_dict = dict()
+    new_dict['top'] = topk
+    new_dict['precision'] = pre_k
+    new_dict['recall'] = rec_k
+    new_dict['f1'] = f1_k
+    new_dict['predict_tag'] = mlb.inverse_transform(np.array([top_idx_one_hot]))
+    new_dict['true_tag'] = mlb.inverse_transform(np.array([label]))
+    to_csv.append(new_dict)
+    logger.info("Loging dict ---> {0}".format(new_dict))
+    return pre_k, rec_k, f1_k
+
+
+def evaluate_batch(pred, label, topk_list=[1, 2, 3, 4, 5], mlb=None):
+    pre = [0.0] * len(topk_list)
+    rc = [0.0] * len(topk_list)
+    f1 = [0.0] * len(topk_list)
+    cnt = 0
+    for i in range(0, len(pred)):
+        for idx, topk in enumerate(topk_list):
+            pre_val, rc_val, f1_val = evaluate_ori(
+                pred=pred[i], label=label[i], topk=topk,mlb=mlb)
+            if pre_val == -1:
+                cnt -= 1
+                break
+            pre[idx] += pre_val
+            rc[idx] += rc_val
+            f1[idx] += f1_val
+        cnt += 1
+    pre[:] = [x / cnt for x in pre]
+    rc[:] = [x / cnt for x in rc]
+    f1[:] = [x / cnt for x in f1]
+    return [pre, rc, f1, cnt]
+
+
 def avg(data):
     import numpy as np
     a = np.array(data)
     res = np.average(a, axis=0)
     return res
-def test(args, model, test_set):
-    batch_size = 500
-    test_data_loader = get_dataloader(
-            test_set, batch_size)
+def test(args, model, test_set,mlb):
+    batch_size = 250
+    test_data_loader = DataLoader(test_set,
+                             batch_size=batch_size,
+                             shuffle=False,
+                             )
     with torch.no_grad():
         model.eval()
         fin_pre = []
@@ -65,7 +136,7 @@ def test(args, model, test_set):
             fin_outputs.extend(torch.sigmoid(
                 outputs).cpu().detach().numpy().tolist())
             [pre, rc, f1, cnt] = evaluate_batch(
-                fin_outputs, fin_targets, [1, 2, 3, 4, 5])
+                fin_outputs, fin_targets, [1, 2, 3, 4, 5],mlb)
             fin_pre.append(pre)
             fin_rc.append(rc)
             fin_f1.append(f1)
@@ -88,7 +159,7 @@ def get_eval_args():
     parser.add_argument(
         "--data_dir", default="../../data/test", type=str,
         help="The input test data dir.")
-    parser.add_argument("--model_path", default="../../data/results/trinity_12-02 15-31-04_t_bert.pt/final_model-364/t_bert.pt", help="The model to evaluate")
+    parser.add_argument("--model_path", default="../../data/results/trinity_12-02 15-31-04_t_bert.pt/final_model-429/t_bert.pt", help="The model to evaluate")
     parser.add_argument("--no_cuda", action="store_true", help="Whether not to use CUDA when available")
     parser.add_argument("--vocab_file", default="../../data/tags/commonTags_post2vec.csv", type=str,
                         help="The tag vocab data file.")
@@ -100,7 +171,7 @@ def get_eval_args():
     args = parser.parse_args()
     return args
 def main():
-    os.environ['CUDA_VISIBLE_DEVICES'] = '2,3'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '2'
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
         datefmt="%m/%d/%Y %H:%M:%S",
@@ -128,7 +199,9 @@ def main():
     if args.model_path and os.path.exists(args.model_path):
         model_path = os.path.join(args.model_path, )
         model.load_state_dict(torch.load(model_path)) 
-    logger.info("model loaded")   
+    logger.info("model loaded")
+    # torch.save(model.module.state_dict(), "./tbert.pt")
+    # logger.info("model saved")
     fin_pre = []
     fin_rc = []
     fin_f1 = []
@@ -141,7 +214,7 @@ def main():
     for file_cnt in range(len(files)):
         logger.info("load file {}".format(file_cnt))
         test_set = load_data_to_dataset(args.mlb, files[file_cnt])
-        [pre, rc, f1, cnt] = test(args, model, test_set)
+        [pre, rc, f1, cnt] = test(args, model, test_set, mlb)
         fin_pre.append(pre)
         fin_rc.append(rc)
         fin_f1.append(f1)
@@ -155,5 +228,11 @@ def main():
     logger.info("Final Precision Score  = {}".format(avg_f1))
     logger.info("Final Count  = {}".format(fin_cnt))
     logger.info("Test finished")
+    keys = to_csv[0].keys()
+
+    with open('./logs/result.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(to_csv)
 if __name__ == "__main__":
     main()
--- bert/util/eval_util.py
+++ bert/util/eval_util.py
@@ -1,4 +1,6 @@
 
+import logging
+logger = logging.getLogger(__name__)
 
 def evaluate_ori(pred, label, topk):
     """
