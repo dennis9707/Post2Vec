7af4b68af40bc8b4b19273031cf12664e06914a4
dennis9707 <he0707@hotmail.com>
2022-01-02 11:24:53 +0800

    feat() added models for no text

--- bert/model/model.py
+++ bert/model/model.py
@@ -92,6 +92,37 @@ class ClassifyHeaderNoTitle(nn.Module):
         return x
 
 
+class ClassifyHeaderNoText(nn.Module):
+    """
+    use averaging pooling across tokens to replace first_token_pooling
+    """
+
+    def __init__(self, config, num_class):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+        self.title_pooler = AvgPooler(config)
+        self.code_pooler = AvgPooler(config)
+
+        # self.dense = nn.Linear(config.hidden_size * 5, config.hidden_size)
+        self.dense = nn.Linear(config.hidden_size * 2, config.hidden_size)
+
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.output_layer = nn.Linear(config.hidden_size, num_class)
+
+    def forward(self, title_hidden, code_hidden):
+        pool_title_hidden = self.title_pooler(title_hidden)
+        pool_code_hidden = self.code_pooler(code_hidden)
+
+        # concatenates the given sequence of tensors in the given dimension
+        concated_hidden = torch.cat((pool_title_hidden, pool_code_hidden), 1)
+
+        x = self.dropout(concated_hidden)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.output_layer(x)
+        return x
+    
 class ClassifyHeaderNoCode(nn.Module):
     """
     use averaging pooling across tokens to replace first_token_pooling
@@ -166,6 +197,28 @@ class TBertTNoTitle(PreTrainedModel):
         logits = self.cls(text_hidden=n_hidden, code_hidden=c_hidden)
         return logits
 
+
+class TBertTNoText(PreTrainedModel):
+    def __init__(self, config, code_bert, num_class):
+        super().__init__(config)
+        self.tbert = AutoModel.from_pretrained(code_bert)
+        self.cbert = AutoModel.from_pretrained(code_bert)
+
+        self.cls = ClassifyHeaderNoText(config, num_class=num_class)
+
+    def forward(
+            self,
+            title_ids=None,
+            title_attention_mask=None,
+            code_ids=None,
+            code_attention_mask=None,
+    ):
+        t_hidden = self.tbert(
+            title_ids, attention_mask=title_attention_mask)[0]
+        c_hidden = self.cbert(code_ids, attention_mask=code_attention_mask)[0]
+
+        logits = self.cls(title_hidden=t_hidden,code_hidden=c_hidden)
+        return logits
 
 class TBertTNoCode(PreTrainedModel):
     def __init__(self, config, code_bert, num_class):
